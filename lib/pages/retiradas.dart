bdc109353f845c09e224cf412ca8f85aafbcfb1d
matheus <matheusssousaribeiro@gmail.com>
2024-10-04 17:49:54 -0300

    add file retiradas.dart

---
+++ lib/pages/retiradas.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutterguys/pages/modules.dart';
+
+class Retiradas extends StatefulWidget {
+  const Retiradas({super.key});
+  
+  RetiradasState createState() => RetiradasState();
+}
+
+class RetiradasState extends State<Retiradas>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+      appBar: appbar(),
+      ),
+    );
+  }
+}
